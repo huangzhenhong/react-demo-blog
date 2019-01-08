1e30eed02082442b70c84ef7b0c100d1f2989424
zhenhong huang <h.dannier@gmail.com>
2019-01-08 15:19:51 -0600

    complete

---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    // console.log(getState());
+
+    // first version
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // using chain
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
+
+// export const fetchPosts = () => {
+//     return async function(dispatch, getState) {
+//         const response = await jsonPlaceholder.get('/posts');
+
+//         dispatch({
+//             type: 'FETCH_POSTS',
+//             payload: response
+//         });
+//     };
+// };
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+// // using lodash memoize function to avoid duplicated request
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import postsReducer from './posts-reducer';
+import usersReducer from './users-reducer';
+
+export default combineReducers({
+    posts: postsReducer,
+    users: usersReducer
+});
